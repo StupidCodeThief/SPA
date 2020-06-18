25e27fe711787d05946e7e21d0cc1e7c1bff427f
StupidCodeThief <denis.vasiliuk@gmail.com>
2020-06-18 13:21:43 +0300

    registration complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -29,13 +29,50 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, email, password } = res.body;
+    const { name, email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
-    } catch (error) {}
 
-    res.send("users route");
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
