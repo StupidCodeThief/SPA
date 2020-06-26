a2eefb2de5fc4652b0c3c385358cd1144e08f913
StupidCodeThief <denis.vasiliuk@gmail.com>
2020-06-26 16:43:38 +0300

    Profile education add amd remove route was done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -211,9 +211,6 @@ router.delete("/experience/:exp_id", authMiddleware, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id });
 
-    console.log(profile.experience[0].id.toString());
-    console.log(req.params.exp_id);
-
     const removeIndex = profile.experience.findIndex(
       (exp) => exp.id.toString() === req.params.exp_id
     );
@@ -223,6 +220,76 @@ router.delete("/experience/:exp_id", authMiddleware, async (req, res) => {
     }
 
     profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route PATCH api/profile/education
+// @desc Add profile education
+// @access Private
+router.patch(
+  "/education",
+  [
+    authMiddleware,
+    [
+      check("school", "School is required").not().isEmpty(),
+      check("degree", "Degree is required").not().isEmpty(),
+      check("fieldofstudy", "Field of study is required").not().isEmpty(),
+      check("from", "From is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const newEdy = ({
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body);
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEdy);
+
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// @route DELETE api/profile/education/:edy_id
+// @desc Delete profile education
+// @access Private
+router.delete("/education/:edy_id", authMiddleware, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.education.findIndex(
+      (edy) => edy.id.toString() === req.params.edy_id
+    );
+
+    if (removeIndex === -1) {
+      return res.status(404).json({ msg: "Education not found" });
+    }
+
+    profile.education.splice(removeIndex, 1);
 
     await profile.save();
     res.json(profile);
